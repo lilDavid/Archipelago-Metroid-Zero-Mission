9c9847bdaf3d0f91eaab65319283a0a80bc77150
NoiseCrush <168460988+NoiseCrush@users.noreply.github.com>
2025-05-21 16:03:40 -0400

    Actually we didn't need that region at all

--- regions.py
+++ regions.py
@@ -72,7 +72,6 @@ def create_regions_and_connections(world: MZMWorld):
     create_region(multiworld, player, "Chozodia Mothership Cockpit", "in the Pirates' mother ship")
     create_region(multiworld, player, "Chozodia Original Power Bomb Room", "in the Pirates' mother ship")
     create_region(multiworld, player, "Chozodia Mecha Ridley Hallway", "in the Pirates' mother ship")
-    create_region(multiworld, player, "Mission Accomplished!")
 
     connect(multiworld, player, "Game Start", "Menu", "Brinstar Start")
     connect(multiworld, player, "Brinstar Start -> Main Shaft", "Brinstar Start", "Brinstar Main", MorphBall.create_rule(world))
@@ -138,4 +137,3 @@ def create_regions_and_connections(world: MZMWorld):
             "Chozodia Mothership Cockpit", deep_mothership_to_cockpit().create_rule(world))
     connect(multiworld, player, "Chozodia Cockpit -> Original PB", "Chozodia Mothership Cockpit", "Chozodia Original Power Bomb Room", cockpit_to_original_pb().create_rule(world))
     connect(multiworld, player, "Chozodia Cockpit -> Mecha Ridley", "Chozodia Mothership Cockpit", "Chozodia Mecha Ridley Hallway", cockpit_to_mecha_ridley().create_rule(world))
-    connect(multiworld, player, "Game Completion", "Chozodia Mecha Ridley Hallway", "Mission Accomplished!", MechaRidleyBoss.create_rule(world))
